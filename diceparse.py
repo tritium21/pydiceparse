cf55ec9159b56e34bea6abd2e140fbd2625f181f
Alexander Walters <tritium@sdamon.com>
2015-05-18 14:44:17 -0400

    need to walk away from this

--- diceparse.py
+++ diceparse.py
@@ -1,100 +1,32 @@
-import collections
-import operator
-import random
-
 import pyparsing as pp
-
-OP_MAP = {
-    '+': operator.add,
-    '-': operator.sub,
-    '*': operator.mul,
-    '/': operator.truediv,
-}
-TARG_MAP = {
-    '<': operator.le,
-    '>': operator.ge,
-}
+assert pp
+from pyparsing import nums, Word, Optional, Each
 
 
-OpTuple = collections.namedtuple('OpTuple', 'partial operator operand')
-RollSpec = collections.namedtuple(
-    'RollSpec',
-    'input pool count sides modifier target'
-)
+integer = Word(nums)
 
+modifier = Word('+-*/', max=1) + integer
+target = Word('<>', max=1) + integer
+suffix = Each([
+    Optional(modifier('modifier'), None),
+    Optional(target('target'), None),
+])
+repete = Optional((integer + '#')('repete'), None)
 
-def make_rollspecs(parser, instring):
-    scanner = parser.scanString(instring)
-    for res, start, end in scanner:
-        st = instring[start:end]
-        print(st, res)
-        yield RollSpec(st, **res)
+pool = '!' + integer('pool')
+reject = integer('reject') + Word(r'\/', max=1)
 
-
-def pool_action(s, loc, tok):
-    return tok[1]
-
-
-def int_action(s, loc, tok):
-    return int(tok[0])
-
-
-def mod_action(MAP):
-    def inner(s, loc, tok):
-        op = tok[0]
-        op_func = MAP[op]
-        second = tok[1]
-
-        def partial(first):
-            return op_func(first, second)
-        return (OpTuple(partial, op, second),)
-    return inner
-
-# DATA TYPES
-integer = pp.Word(pp.nums).setParseAction(int_action)
-
-# MODIFIERS
-mod_op = pp.Or(list(OP_MAP.keys()))
-modifier = (mod_op + integer).setParseAction(mod_action(OP_MAP))
-modifier = pp.Optional(modifier('modifier'), None)
-
-# TARGET
-targ_op = pp.Or(list(TARG_MAP.keys()))
-target = (targ_op + integer).setParseAction(mod_action(TARG_MAP))
-target = pp.Optional(target('target'), None)
-
-# POOL
-pool = pp.Optional(('!' + integer).setParseAction(pool_action)('pool'), None)
-
-# BASIC SYNTAX
 basic = integer('count') + 'd' + integer('sides')
 
-# BTING IT ALL TOGETHER
-roll = basic + pool + modifier + target
-
-
-def roller(rollspec):
-    success = None
-    rolls = [random.randint(1, rollspec.sides) for _ in range(rollspec.count)]
-    if rollspec.pool:
-        total = sum(x >= rollspec.pool for x in rolls)
-        if rollspec.modifier:
-            total = rollspec.modifier.partial(total)
-        if rollspec.target:
-            success = rollspec.target.partial(total)
-        else:
-            success = total >= 1
-    else:
-        total = sum(rolls)
-        if rollspec.modifier:
-            total = rollspec.modifier.partial(total)
-        if rollspec.target:
-            success = rollspec.target.partial(total)
-    return rollspec, rolls, total, success
-
+with_pool = repete + basic + pool + suffix
+with_reject = repete + reject + basic + suffix
+standard = repete + basic + suffix
+roll = with_pool ^ with_reject ^ standard
 
 if __name__ == '__main__':
     import sys
     argv = ' '.join(sys.argv[1:])
-    for spec in make_rollspecs(roll, argv):
-        print(roller(spec))
+    scanner = standard.scanString(argv)
+    for res, start, end in scanner:
+        st = argv[start:end]
+        print(st, res.asDict())
