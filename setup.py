2be15996ba08bf50093c02691427a8857b653cf7
Alexander Walters <tritium@sdamon.com>
2015-05-19 15:56:21 -0400

    Setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pydiceparse",
+    version="0.1",
+    author="Alex Walters",
+    author_email="tritium@sdamon.com",
+    description=("Dice syntax parser and roller"),
+    license="BSD",
+    modules="diceparse",
+)
