25414cc7a246aa1e4ca72772f1b3baa25b136615
Alexander Walters <tritium@sdamon.com>
2015-05-19 13:22:54 -0400

    It works!

--- diceparse.py
+++ diceparse.py
@@ -1,7 +1,13 @@
 from collections import namedtuple
+from heapq import nlargest, nsmallest
+from functools import partial
 from operator import add, sub, ge, le
+from random import randint
 
-from pyparsing import Literal, nums, NotAny, Optional, Or, Word, WordStart
+from pyparsing import (
+        CaselessLiteral as Literal,
+        nums, NotAny, Optional, Or, Word, WordStart
+)
 
 
 def make_optional(parser, action, name):
@@ -23,15 +29,20 @@ fate_type = integer('count') + Literal('df')('type') + NotAny(integer)
 # Operator base
 Operation = namedtuple('Operation', 'partial operator operand')
 
-def operator_action(mapping):
+def operator_action(mapping, swap=False):
     def closure(s, loc, tok):
-        operator = tok[0]
-        operand = tok[1]
+        seq = tok[:2] if not swap else tok[:2][::-1]
+        operator = seq[0]
+        operand = second = seq[1]
         f = mapping[operator]
 
-        def partial(first):
-            return f(first, second)
-        return Operation(partial, operator, operand)
+        if swap:
+            fp = partial(f, second)
+        else:
+            def fp(first):
+                return f(first, second)
+        return (Operation(fp, operator, operand),)
+    return closure
 
 # Modifiers
 modifier_map = {'+': add, '-': sub}
@@ -56,8 +67,11 @@ def pos_action(pos):
     return action
 
 repete, _ = make_optional(integer + Literal('#'), pos_action(0), 'repete')
+
+reject_mapping = {'\\': nlargest, '/': nsmallest}
+reject_action = operator_action(reject_mapping, swap=True)
 reject, no_reject = make_optional(
-    integer + Word(r'\/', max=1), pos_action(0), 'reject'
+    integer + Word(r'\/', max=1), reject_action, 'reject'
 )
 pool, no_pool = make_optional(Literal('/') + integer, pos_action(1), 'pool')
 explode, no_explode = make_optional(
@@ -74,6 +88,8 @@ roll = WordStart() + Or([standard, pool, fate])
 KEYS = 'count type sides modifier compare repete reject pool explode'.split()
 PROTOTYPE = dict.fromkeys(KEYS)
 RollSpec = namedtuple('RollSpec', ['rollstr'] + KEYS)
+ResultSpec = namedtuple('ResultSpec', 'rolls total success')
+Result = namedtuple('Result', 'rollspec results total')
 
 
 def validate(rollstr, result, prototype=None):
@@ -83,10 +99,64 @@ def validate(rollstr, result, prototype=None):
     return RollSpec(rollstr, **values)
 
 
+def fate_roller(rs):
+    rolls = [randint(-1, 1) for _ in range(rs.count)]
+    total = sum(rolls)
+    if rs.modifier:
+        total = rs.modifier.partial(total)
+    success = total >= 1
+    spec = ResultSpec(rolls, total, success)
+    return spec
+
+
+def explode(rolls, sides, trigger):
+    newrolls = []
+    _last = rolls
+    while True:
+        e = sum(d>=trigger for d in _last)
+        if e:
+            _last = [randint(1, sides) for _ in range(e)]
+            newrolls.extend(_last)
+            continue
+        break
+    return newrolls
+
+
+def standard_roller(rs):
+    rolls = [randint(1, rs.sides) for _ in range(rs.count)]
+    success = None
+    if rs.reject:
+        rolls = rs.reject.partial(rolls)
+    if rs.pool:
+        if rs.explode:
+            rolls.extend(explode(rolls, rs.sides, rs.explode))
+        total = sum(d>=rs.pool for d in rolls)
+        success = total>=1
+    else:
+        total = sum(rolls)
+    if rs.modifier:
+        total = rs.modifier.partial(total)
+    if rs.compare:
+        success = rs.compare.partial(total)
+    return ResultSpec(rolls, total, success)
+
+
+def roller(rs):
+    typemap = {'df': fate_roller, 'd': standard_roller}
+    repete = range(rs.repete if rs.repete is not None else 1)
+    _results = []
+    _total = 0
+    for _ in repete:
+        spec = typemap[rs.type](rs)
+        _results.append(spec)
+        _total += spec.total
+    return Result(rs, _results, _total)
+
+
 if __name__ == '__main__':
     import sys
     argv = ' '.join(sys.argv[1:])
     scanner = roll.scanString(argv)
     for res, start, end in scanner:
         st = argv[start:end]
-        print(validate(st, res))
+        print(roller(validate(st, res)))
