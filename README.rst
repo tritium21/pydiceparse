b6593cb471d254d7dcc966cd9a36da81b338d901
Alexander Walters <tritium@sdamon.com>
2015-05-31 18:52:17 -0400

    fixed setup, filled README some

---
+++ README.rst
@@ -0,0 +1,7 @@
+pydiceparse
+===========
+
+This is a simple module to parse dice syntax, and return a [sequence of]
+strings, suitable for use in IRC bots.
+
+Documentation to come.
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
+
+def read(fname):
+    with open(fname, 'r') as f:
+        return f.read()
+
+
 setup(
     name="pydiceparse",
-    version="0.1",
+    version="0.1.1",
     author="Alex Walters",
     author_email="tritium@sdamon.com",
     description=("Dice syntax parser and roller"),
+    long_description=read('README.rst'),
+    url='https://github.com/tritium21/pydiceparse',
     license="BSD",
     py_modules=["diceparse"],
     install_requires=[
@@ -15,5 +23,15 @@ setup(
         'console_scripts': {
             'pydice = diceparse:main'
         }
-    }
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
 )
