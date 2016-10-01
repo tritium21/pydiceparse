7c129c23678c71645dccb0d756b7cb6a236d6f65
alex walters <tritium@sdamon.com>
2016-10-01 13:48:07 -0400

    Version Number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="pydiceparse",
-    version="0.3.5",
+    version="0.3.6",
     author="Alex Walters",
     author_email="tritium@sdamon.com",
     description=("Dice syntax parser and roller"),
