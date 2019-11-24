e1cc72c2d3caa481438790acd5393db2d7ea6f9e
Alex Walters <1781587+tritium21@users.noreply.github.com>
2019-11-23 21:09:27 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="pydiceparse",
-    version="0.4.0",
+    version="0.4.1",
     author="Alex Walters",
     author_email="tritium@sdamon.com",
     description=("Dice syntax parser and roller"),
