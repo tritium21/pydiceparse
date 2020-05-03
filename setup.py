0254a1781a738a1a57adf56a0f7b97bcd16829d4
alex walters <1781587+tritium21@users.noreply.github.com>
2020-05-03 02:00:52 -0400

    Bug in setup.py...

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ def read(fname):
 
 setup(
     name="pydiceparse",
-    version="0.5.1.0",
+    version="0.5.1.1",
     author="Alex Walters",
     author_email="tritium@sdamon.com",
     description=("Dice syntax parser and roller"),
     long_description=read('README.rst'),
     url='https://github.com/tritium21/pydiceparse',
     license="BSD",
-    py_modules=["diceparse", "deck"],
+    py_modules=["diceparse", "deck", "table"],
     install_requires=[
         'lark-parser>=0.6.3'
     ],
